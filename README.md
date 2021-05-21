c8727e002df0d1f279e44d3a7efa29e2ac4b73d9
José Reveco <offjose@gmail.com>
2021-05-21 19:45:32 -0400

    quitandoCosas

---
+++ README.md
@@ -0,0 +1,4 @@
+#LaWeApp
+G2
+Colocar datos.txt en la carpeta raiz.
+Colocar resources y recursos en src y en el bin
--- src/module-info.java
+++ src/module-info.java
@@ -7,7 +7,7 @@ module LaWeAppFX {
 	requires javafx.media;
 	requires java.logging;
 	
-	opens application to javafx.graphics, javafx.fxml;
+	//opens application to javafx.graphics, javafx.fxml;
 	opens modelo to javafx.graphics, javafx.fxml;
 	opens logica to javafx.graphics, javafx.fxml;
 }
